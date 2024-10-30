69f0d502178c98cffb714758a2dbcddab5bed67e
profolyo <b0443634@gmail.com>
2024-10-30 20:23:01 +0200

    fixd

--- javascript/28-10-2024/script.js
+++ javascript/28-10-2024/script.js
@@ -1,1 +1,39 @@
+//string;
 console.log("Hola from Javascript-File 1");
+
+
+
+//number;
+console.log(6 + 6) // 12
+console.log("6" + "6") // "66"
+console.log("6" + 6 + 6) // "666"
+
+// + - * /
+
+console.log("**********************")
+//משתנה
+var mynumber = 50;
+
+console.log(mynumber + 50) // 100
+console.log(mynumber) // 50
+
+mynumber = mynumber + 50;
+console.log(mynumber); // 100
+
+mynumber += 50;
+
+//boolean: true, false
+console.log(mynumber == 50); //false
+console.log(mynumber == 150); //true
+console.log(mynumber == "150"); //true
+console.log(mynumber === "150"); //false
+
+console.log("**********************");
+console.log(true == 1); // true
+console.log(true === 1); // false
+
+
+
+
+
+
