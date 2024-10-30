dbe8cc5fb308d676b03d02c00bdd14f33d9157d6
profolyo <b0443634@gmail.com>
2024-10-30 21:11:43 +0200

    rjgjf

--- javascript/28-10-2024/script.js
+++ javascript/28-10-2024/script.js
@@ -28,12 +28,31 @@ console.log(mynumber == 150); //true
 console.log(mynumber == "150"); //true
 console.log(mynumber === "150"); //false
 
-console.log("**********************");
+console.log("*********************************");
 console.log(true == 1); // true
 console.log(true === 1); // false
 
+console.log(9 % 2); // 1 (9 / 2 = נשאר 1)
+console.log(7 / 4); // 1.75
+console.log(7 % 4); // 3 (7 % 4 = נשאר 3)
+
+console.log(4 % 2 == 0); // true
 
 
+console.log("*********************************");
 
+var myNumber2 = 10;
+
+//method 1
+myNumber2 = myNumber2 + 1;
+
+//method 2
+myNumber2 += 1;
+
+//method 3
+myNumber2++;
+
+
+console.log("*********************************");
 
 
