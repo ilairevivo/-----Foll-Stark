40a3fb119424e0310e8b407dedbcabfe430f3c3a
ilai <b0443634@gmail.com>
2025-02-10 12:04:33 +0200

    movin folder in new folder

--- javascript/19-12-2024-Modules/index.js
+++
@@ -1,8 +0,0 @@
-import { changeCar, printCar } from "./car.js";
-
-console.log("******************* before change *********************");
-printCar();
-changeCar('Toyota', 'Corolla', 2020);
-
-console.log("******************* after change *********************");
-printCar();
--- javascript/28-10-2024/script2.js
+++
@@ -1,1 +0,0 @@
-console.log("Hola from Javascript-File 2");
